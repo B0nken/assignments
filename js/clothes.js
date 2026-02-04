ddc47322710f4066c18cb9ee790916a396eab5fd
Stellan <Stellan.benckert@gmail.com>
2026-02-04 08:40:07 +0100

    clothes.js added and exported products

---
+++ js/clothes.js
@@ -0,0 +1,107 @@
+export const clothes = [
+    {
+        id: 0
+        ,
+        name: "Sweater"
+        ,
+        desc: "Comfortable knitted sweater"
+        ,
+        image: "img/0.png"
+        ,
+        category: "tops"
+
+    },
+
+    {
+        id: 1
+        ,
+        name: "Hat"
+        ,
+        desc: "Fitted knitted hat"
+        ,
+        image: "img/1.png"
+        ,
+        category: "accessories"
+
+    },
+
+    {
+        id: 2
+        ,
+        name: "Shoes"
+        ,
+        desc: "Quality sneakers"
+        ,
+        image: "img/2.png"
+        ,
+        category: "footwear"
+
+    },
+
+    {
+        id: 3
+        ,
+        name: "Pants"
+        ,
+        desc: "Fitted straight pants"
+        ,
+        image: "img/3.png"
+        ,
+        category: "bottom"
+
+    },
+
+    {
+        id: 4
+        ,
+        name: "Scarf"
+        ,
+        desc: "Knitted soft scarf"
+        ,
+        image: "img/4.png"
+        ,
+        category: "accessories"
+
+    },
+
+    {
+        id: 5
+        ,
+        name: "Shirt"
+        ,
+        desc: "Loose buttoned shirt"
+        ,
+        image: "img/5.png"
+        ,
+        category: "tops"
+
+    },
+
+    {
+        id: 6
+        ,
+        name: "Gloves"
+        ,
+        desc: "Knitted warm gloves"
+        ,
+        image: "img/6.png"
+        ,
+        category: "accessories"
+
+    },
+
+    {
+        id: 7
+        ,
+        name: "Socks"
+        ,
+        desc: "Quality comfortable socks"
+        ,
+        image: "img/7.png"
+        ,
+        category: "accessories"
+
+    },
+
+
+]
