6b8e39aee093620f87b441c9302f374910a2a025
Stellan <Stellan.benckert@gmail.com>
2026-02-05 13:20:35 +0100

    Images comprimised nad fixed. Html for assignment 2 is done. and some changes to clothes.js added.

--- js/assignment.js
+++ js/assignment.js
@@ -3,13 +3,13 @@ export const assignmentsData = [
     id: "assignment",
     title: "Uppgift 1",
     link: "assignment1/assignment1.html",
-    desc: "Skapa en miniräknare med hjälp av JavaScript"
+    desc: "Skapa en landningssida"
 },
 
 {
     id: "assignment2",
     title: "Uppgift 2",
     link: "assignment1/assignment2.html",
-    desc: "Skapa ett flipperspel med JavaScript"
+    desc: "Skapa en hemsida med produkter"
 }
 ];
