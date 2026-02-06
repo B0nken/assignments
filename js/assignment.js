95a7d2285b1fcbd78872697ac3c5f44f8c85caea
Stellan <Stellan.benckert@gmail.com>
2026-02-06 10:31:26 +0100

    once again fixed

--- js/assignment.js
+++ js/assignment.js
@@ -2,14 +2,14 @@ export const assignmentsData = [
     {
     id: "assignment",
     title: "Uppgift 1",
-    link: "./assignments/assignment1/assignment1.html",
+    link: "assignment1/assignment1.html",
     desc: "Skapa en landningssida"
 },
 
 {
     id: "assignment2",
     title: "Uppgift 2",
-    link: "./assignments/assignment1/assignment2.html",
+    link: "assignment1/assignment2.html",
     desc: "Skapa en hemsida med produkter"
 }
 ];
