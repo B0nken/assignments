35918ea400d67cc5d3199d2368f7ea5d7b29bbb7
Stellan <Stellan.benckert@gmail.com>
2026-03-05 12:14:25 +0100

    html of assignment3 now linked to fight.js and exported class added to fight.js

---
+++ js/fight.js
@@ -0,0 +1,15 @@
+export class Fight {
+    #p1;
+    #p2;
+    #winner;
+    #loser;
+    #element;
+
+    constructor(p1, p2) {
+        this.#p1 = p1;
+        this.#p2 = p2;
+        this.#winner = null;
+    }
+
+
+}
