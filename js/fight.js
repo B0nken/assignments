5d5686a5ce51b8e050870ebb92aebfc258ce1f6f
Stellan <Stellan.benckert@gmail.com>
2026-03-05 12:26:55 +0100

    gets added for if a match has been played and who the winner is.

--- js/fight.js
+++ js/fight.js
@@ -1,5 +1,5 @@
 export class Fight {
-    #p1;
+    #p1; //private encapsulated fields so no other files change the winners or players
     #p2;
     #winner;
     #loser;
@@ -8,8 +8,15 @@ export class Fight {
     constructor(p1, p2) {
         this.#p1 = p1;
         this.#p2 = p2;
-        this.#winner = null;
+        this.#winner = null; //no winner at the start
     }
 
+    get played() {
+        return this.#winner !== null; //returns true if there is a winner
+    }
+
+    get winner() {
+        return this.#winner //returns the entire object for winner
+    }
 
 }
