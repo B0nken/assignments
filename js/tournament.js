d30d44833cfde6115d1d3af9ec70e62888c6581c
Stellan <Stellan.benckert@gmail.com>
2026-03-07 08:37:08 +0100

    fetch completed with error message

--- js/tournament.js
+++ js/tournament.js
@@ -8,6 +8,22 @@ const bracketCont = document.getElementById("bracket")
 
 
 async function contestantsFetch() {
+    try {
+
+        const response = await fetch('guitarists.json');
+        fighters = await response.json();
+        console.log("Fighters fetched", fighters)
+    } catch (error){
+console.log("Error", error)
+    }
+}
     
+
+function start() {
+    bracketCont.innerHTML = "";
+    fightNum = 1;
+
     
 }
+
+
