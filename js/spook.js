a0953e1961018828cc3ff1768b387e8e8e6cf385
stellan <stellan.benckert@gmail.com>
2026-04-24 11:26:04 +0200

    renderHouse function added to init and completed with logic to print all houses with name, price and a link that takes you to its location in house.html

--- js/spook.js
+++ js/spook.js
@@ -13,6 +13,26 @@ async function init() {
     } catch (error) {
         console.error("Something went wrong when getting the data", error)
     }
+    renderHouse(houses)
 }
+
+function renderHouse(houses) {
+    const cont = document.getElementById("list")
+    cont.innerHTML = ""
+console.log(houses.length)
+    
+houses.forEach(house => {
+    const card = document.createElement("div")
+
+    card.innerHTML = `
+    <img src="/assignment4/img/${house.image}" alt="${house.name}">
+    <h3>${house.name}</h3>
+    <p> Pris: ${house.pricePerNight} kr</p>
+    <a href="/assignment4/house.html?id=${house.id}">Läs mer</a>`
+
+    cont.appendChild(card)
+});
+}
+
 
 init();
